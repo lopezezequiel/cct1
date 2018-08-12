9e0216753a06fda99abea28d2d2704ceab22a231
López Ricardo Ezequiel <mail@lopezezequiel.com>
2018-08-12 16:44:58 -0300

    Enrutando / a index.html

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000, function () {
