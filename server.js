651a7d460a71744df34459d9de102fd30d7af71c
López Ricardo Ezequiel <mail@lopezezequiel.com>
2018-08-12 17:09:01 -0300

    Agregado enrutamiento al sender

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/sender', function (req, res) {
+  res.sendFile(path.join(__dirname, 'sender.html'));
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
