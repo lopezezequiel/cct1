426dfe4f0d1ccbdc9115fc13d337bfe03dc7a2dc
López Ricardo Ezequiel <mail@lopezezequiel.com>
2018-08-12 17:51:43 -0300

    Prueba sender de https://github.com/googlecast/CastVideos-chrome

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.use('public', express.static('public'));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/sender', function (req, res) {
   res.sendFile(path.join(__dirname, 'sender.html'));
+});
+
+app.get('/sender2', function (req, res) {
+  res.sendFile(path.join(__dirname, 'sender2.html'));
 });
 
 app.listen(process.env.PORT || 3000, function () {
